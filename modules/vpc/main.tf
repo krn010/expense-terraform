ef38e63ea1be3efd2b812c906fa8aff89f79d264
metal <metalheadkrn@gmail.com>
2024-10-07 21:21:55 +0530

    Creating Basic VPC through terraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,3 +8,35 @@ resource "aws_vpc" "main" {
 
 
 
+resource "aws_subnet" "main" {
+  count      = length(var.subnets_cidr)
+  cidr_block = element(var.subnets_cidr, count.index )
+  availability_zone = element(var.az, count.index )
+
+  tags = {
+    Name = "subnet-${count.index}"
+  }
+}
+
+resource "aws_vpc_peering_connection" "main" {
+
+  vpc_id        = aws_vpc.main.id
+  peer_vpc_id   = data.aws_vpc.default.id
+  auto_accept = true
+
+  tags = {
+    Name = "${var.env}-vpc-with-default-vpc"
+  }
+}
+
+resource "aws_route" "main" {
+  route_table_id            = aws_vpc.main.main_route_table_id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = aws_vpc.main.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
