89575a0c27c2ad44d44e6dee5059d72e2dd16578
metal <metalheadkrn@gmail.com>
2024-10-08 22:03:52 +0530

    Creating Basic VPC, subnets, peering connections, instances , internet gateways,nat gateway through terraform

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -71,11 +71,12 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_eip" "main" {
-
-  domain   = "vpc"
+  count         = length(var.public_subnets_cidr)
+  domain        = "vpc"
 }
+
 resource "aws_nat_gateway" "main" {
-  count      = length(var.public_subnets_cidr)
+  count         = length(var.public_subnets_cidr)
   allocation_id = lookup(element(aws_eip.main, count.index ), "id", null)
   subnet_id     = lookup(element(aws_subnet.public, count.index ), "id", null)
 
